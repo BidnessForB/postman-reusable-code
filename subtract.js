7d5b5eb9ed77b8ce284ee16017fd9d9cac264da5
Bryan Cross <bryanrkcross@gmail.com>
2023-12-02 08:20:21 -0600

    Update subtract.js

--- subtract.js
+++ subtract.js
@@ -1,3 +1,5 @@
-function func(num, val) {
-  return num - val;
-}
+//We use a generic function name here so that we can use the same code to import and eval many functions
+exports.subtractNumber = function (num, val) {
+    return num - val;
+  }
+  module.exports = exports;
